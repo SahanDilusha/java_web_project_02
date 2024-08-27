c7b2fc590067d42e990735a8a3c324f95f07d0df
SahanDilusha <sahancodelygroup@gmail.com>
2024-08-27 17:31:33 +0530

    create uerRegistration()

--- web/js/script.js
+++ web/js/script.js
@@ -1,5 +1,5 @@
 function uerRegistration() {
-    
+
     var mobile = document.getElementById("mobile").value;
     console.log(mobile);
     var name = document.getElementById("name").value;
@@ -12,11 +12,24 @@ function uerRegistration() {
     console.log(mobile);
     var gender = document.getElementById("male").checked ? "Male" : "Female";
     console.log(gender);
-    
+
     var obj = {mobile: mobile, name: name, country: country, password: password, gender: gender};
-    
+
     var jsonObj = JSON.stringify(obj);
-    
     console.log(jsonObj);
-    
+
+    var request = new XMLHttpRequest();
+ 
+    request.onreadystatechange = function () {
+
+        if (request.readyState == 4 && request.status == 200) {
+            
+        }
+
+    }
+
+    request.open("POST", "User_Registration", true);
+    request.setRequestHeader("Content-Type","application/json");
+    request.send(jsonObj);
+
 }
