39f63b83a7d9e3947377402e94f3c7e339092abb
SahanDilusha <sahancodelygroup@gmail.com>
2024-08-27 15:12:52 +0530

    create uerRegistration()

--- web/js/script.js
+++ web/js/script.js
@@ -1,10 +1,22 @@
 function uerRegistration() {
 
     var mobile = document.getElementById("mobile").value;
+    console.log(mobile);
     var name = document.getElementById("name").value;
+    console.log(name);
     var country = document.getElementById("country").value;
+    console.log(country);
     var password = document.getElementById("password").value;
+    console.log(password);
     var mobile = document.getElementById("mobile").value;
+    console.log(mobile);
 
+    var gender;
+
+    if (document.getElementById("male").checked) {
+        gender = "Male";
+    } else {
+        gender = "Female";
+    }
 
 }
