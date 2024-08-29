7ed2e545b69b50b10aa8ba1e21bc713f6cdfc761
SahanDilusha <sahancodelygroup@gmail.com>
2024-08-29 09:17:20 +0530

    create uerRegistration()

--- src/java/contrroler/User_Registration.java
+++ src/java/contrroler/User_Registration.java
@@ -2,6 +2,7 @@ package contrroler;
 
 import com.google.gson.Gson;
 import java.io.IOException;
+import java.util.HashMap;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,14 +16,22 @@ public class User_Registration extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Gson gson = new Gson();
-        
+
         User user = gson.fromJson(req.getReader(), User.class);
-        
+
         System.out.println(user.getMobile());
         System.out.println(user.getName());
-        
-    }
 
-    
+        HashMap<String, User> userMap = (HashMap<String, User>) req.getServletContext().getAttribute("userMap");
+
+        if (userMap.containsKey(user.getMobile())) {
+            resp.sendRedirect("user_registration.jsp?msg=error1");
+        } else {
+            
+           
+           
+        }
+
+    }
 
 }
