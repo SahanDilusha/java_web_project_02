421fac777f75a1774ef2200ad76f1773fff93c60
SahanDilusha <sahancodelygroup@gmail.com>
2024-08-29 09:08:31 +0530

    create uerRegistration()

---
+++ src/java/model/Filter1.java
@@ -0,0 +1,38 @@
+package model;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter (urlPatterns = {"/index.jsp"})
+public class Filter1 implements Filter{
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        
+        if (req.getSession().getAttribute("user") == null) {
+            res.sendRedirect("user_login.jsp");
+        }else{
+            chain.doFilter(request, response);
+        }
+        
+    }
+
+    @Override
+    public void destroy() {
+    }
+    
+}
---
+++ src/java/model/Listener1.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.util.HashMap;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class Listener1 implements ServletContextListener{
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+
+        HashMap<String,User> userMap = new HashMap<String, User>();
+        sce.getServletContext().setAttribute("userMap", userMap);
+        
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
+    
+}
